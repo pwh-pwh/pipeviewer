cda30f58fb98f5ddef555133d4763223ee8caa4b
pwh-pwh <plmoknpwh@163.com>
2023-04-23 10:53:17 +0800

    use modules

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,21 @@
-use clap::{App, Arg};
-use std::fs::File;
-use std::io::{BufReader, BufWriter, ErrorKind, Read, Result, Write};
-use std::{env, io};
+use std::io::Result;
+use pipeviewer::{args::Args,write,read,stats};
 
-const CHUNK_SIZE: usize = 16 * 1024;
 
 fn main() -> Result<()> {
-    let matches = App::new("pipeviewer")
-        .arg(Arg::with_name("infile").help("Read from a file instead of stdin"))
-        .arg(
-            Arg::with_name("outfile")
-                .short("o")
-                .long("outfile")
-                .takes_value(true)
-                .help("Write output to a file instead of stdout"),
-        )
-        .arg(Arg::with_name("silent").short("s").long("silent"))
-        .get_matches();
-    let infile = matches.value_of("infile").unwrap_or_default();
-    let outfile = matches.value_of("outfile").unwrap_or_default();
-    let silent = if matches.is_present("silent") {
-        true
-    } else {
-        !env::var("PV_SILENT").unwrap_or(String::new()).is_empty()
-    };
-    let mut reader: Box<dyn Read> = if !infile.is_empty() {
-        Box::new(BufReader::new(File::open(infile)?))
-    } else {
-        Box::new(BufReader::new(io::stdin()))
-    };
-    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
-        Box::new(BufWriter::new(File::create(outfile)?))
-    } else {
-        Box::new(BufWriter::new(io::stdout()))
-    };
-
+    let args = Args::parse();
     let mut total_bytes = 0;
-    let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let num_read = match reader.read(&mut buffer) {
-            Ok(0) => break,
-            Ok(num) => num,
+        let buffer = match read::read(&args.infile) {
+            Ok(x) if x.is_empty() => break,
+            Ok(x) => x,
             Err(_) => break,
         };
-        total_bytes += num_read;
-        if !silent {
-            eprint!("\rtotal_bytes:{}", total_bytes);
-        }
-        if let Err(e) = writer.write_all(&buffer[..num_read]) {
-            if e.kind() == ErrorKind::BrokenPipe {
-                break;
-            }
-            return Err(e);
+        stats::stats(args.silent,buffer.len(),&mut total_bytes,false);
+        if !write::write(&args.outfile,&buffer)? {
+            break
         }
     }
+    stats::stats(args.silent,0,&mut total_bytes,true);
     Ok(())
 }
---
+++ src/write.rs
@@ -0,0 +1,18 @@
+use std::fs::File;
+use std::io;
+use std::io::{BufWriter, ErrorKind, Result, Write};
+pub fn write(outfile:&str,buffer:&[u8]) ->Result<bool> {
+    let mut writer: Box<dyn Write> = if !outfile.is_empty() {
+        Box::new(BufWriter::new(File::create(outfile)?))
+    } else {
+        Box::new(BufWriter::new(io::stdout()))
+    };
+    if let Err(e) = writer.write_all(buffer) {
+        if e.kind() == ErrorKind::BrokenPipe {
+            return Ok(false);
+        } else {
+            return Err(e);
+        }
+    }
+    Ok(true)
+}
